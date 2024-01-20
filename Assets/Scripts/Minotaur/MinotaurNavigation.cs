08c7df4f0b299f5f5e3c78d123eb12fb54aea6b0
Emilien-T <emilien.taisne@mail.mcgill.ca>
2024-01-20 14:42:20 -0500

    finished minotaur

--- Assets/Scripts/Minotaur/MinotaurNavigation.cs
+++ Assets/Scripts/Minotaur/MinotaurNavigation.cs
@@ -13,20 +13,22 @@ public class MinotaurNavigation : MonoBehaviour
     public bool isPatroling;
     public List<Transform> patrolCheckPoints;
     private NavMeshAgent agent;
-    private bool isHiding;
+    private bool gotJucked;
     private int patrolInteger;
+    private Vector3 previousDestination;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         patrolInteger = 0;
+        gotJucked = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Check if has caught player (less than 1 meter away and not hiding)
-        if(Vector3.Distance(transform.position,player.transform.position) < 3 && !isHiding){
+        if(Vector3.Distance(transform.position,player.transform.position) < 3 && !FPSController.Instance.IsHiding){
             Debug.Log("Dead");
             logic.lose();
         }
@@ -41,12 +43,17 @@ public class MinotaurNavigation : MonoBehaviour
         }
         else{
             //If hiding move to despawn
-            if(isHiding){
+            if(FPSController.Instance.IsHiding){
                 if(agent.remainingDistance<0.4){
+                    Debug.Log("Jucked");
                     //should pass infront of the player's hiding spot
+                    previousDestination = agent.destination;
                     agent.destination = transform.position + transform.forward*100;
-                    Destroy(gameObject,3);
-                } 
+                    gotJucked = true;
+                }
+                if(Vector3.Distance(transform.position,previousDestination) > 10 && gotJucked){
+                    Destroy(gameObject);
+                }
             }
             else{
                 ChasePlayer();
