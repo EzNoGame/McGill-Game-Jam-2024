be666c7f859effc40eb61d4a0365255dc4a2cac6
EzNoGame <ez041126@gmail.com>
2024-01-20 00:13:52 -0500

    Basic main menu

--- Assets/Scripts/MainMenu/MenuButtonManager.cs
+++ Assets/Scripts/MainMenu/MenuButtonManager.cs
@@ -3,44 +3,26 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtonManager : Singleton<MenuButtonManager>
 {
-    public GameObject ButtonMenu;
-    public GameObject ContinueButton;
-    public GameObject ComfirmPopup;
-    public GameObject CommanderSelectionMenu;
-
-    public bool NotMainMenu;
-    
-    [SerializeField]
-    private Transform MenuParent;
-
     [SerializeField]
     private AudioClip _mainMenuBGM;
 
     private void Start()
     {
-        if(NotMainMenu)
-            return;
-        
         SaveSystem.Instance.NewRun();
-            
-        ButtonMenu.SetActive(true);
-
         AudioSystem.Instance.PlayBGM(_mainMenuBGM);
     }
 
-    public void NewRun()
+    public void Begin()
     {
-        if(ContinueButton && ContinueButton.activeSelf)
-            Instantiate(ComfirmPopup, MenuParent);
-        else
-            Instantiate(CommanderSelectionMenu, MenuParent);
+        //TODO: Start the game
+        SceneManager.LoadScene("Main");
     }
 
     // public void Continue()
     // {
     //     SceneTransitionSystem.Instance.LoadSceneTransist("Map");
     // }
-    public void Option() => SceneManager.LoadScene("Option", LoadSceneMode.Additive);
+    public void Settings() => SceneManager.LoadScene("Settings", LoadSceneMode.Additive);
     public void Menu()
     {
         Destroy(gameObject);
