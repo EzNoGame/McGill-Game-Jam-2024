829b9c6d227abd5d404e708264b8015461515251
averystraumann <averystraumann@icloud.com>
2024-01-20 16:11:45 -0500

    elevator shift on puzzle complete

--- Assets/Scripts/Puzzle/Puzzle.cs
+++ Assets/Scripts/Puzzle/Puzzle.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public abstract class Puzzle : MonoBehaviour
 {
     [SerializeField]
     private int ID;
     protected bool _solved = false;
+    public UnityEvent onPuzzleSolved;
     public int GetID() => ID;
 
     protected virtual void Solved()
     {
         SaveSystem.Instance.AddPuzzle(ID);
+        Debug.Log("solved");
+        onPuzzleSolved.Invoke();
         _solved = true;
     }
+
+
 }
