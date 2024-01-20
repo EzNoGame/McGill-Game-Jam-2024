4f263c5aff8efd19dd3ff31084b2e8e61c9af28c
EzNoGame <ez041126@gmail.com>
2024-01-20 12:16:28 -0500

    reopen combination lock

--- Assets/Scripts/Combination Lock/CombinationLock.cs
+++ Assets/Scripts/Combination Lock/CombinationLock.cs
@@ -9,6 +9,11 @@ public class CombinationLock : Puzzle
 
     private List<int> _userInput;
 
+    [SerializeField]
+    private List<CombinationLockLight> _lights;
+    [SerializeField]
+    private Material _passMaterial, _failMaterial;
+
     void OnEnable()
     {
         BroadcastSystem.KeyPressed += AcceptKey;
@@ -24,18 +29,35 @@ public class CombinationLock : Puzzle
         _userInput = new List<int>();
     }
 
-
     private void AcceptKey(int _keyValue)
     {
+        if(_solved)
+            return;
+
         Debug.Log("Key pressed: " + _keyValue);
 
         _userInput.Add(_keyValue);
+        _lights[_userInput.Count - 1].TurnOn();
         
         if(_userInput.SequenceEqual(_combination))
         {
             BroadcastSystem.LockUnlocked?.Invoke(this);
             Solved();
             Debug.Log("Lock Unlocked");
+            foreach (var light in _lights)
+            {
+                light.gameObject.GetComponent<Renderer>().material = _passMaterial;
+            }
+            _solved = true;
+        }
+        else if(_userInput.Count == _lights.Count)
+        {
+            Debug.Log("Wrong combination!");
+            foreach (var light in _lights)
+            {
+                light.gameObject.GetComponent<Renderer>().material = _failMaterial;
+            }
+            _userInput.Clear();
         }
     }
 }
--- Assets/Scripts/Puzzle/Puzzle.cs
+++ Assets/Scripts/Puzzle/Puzzle.cs
@@ -6,7 +6,7 @@ public abstract class Puzzle : MonoBehaviour
 {
     [SerializeField]
     private int ID;
-
+    protected bool _solved = false;
     public int GetID() => ID;
 
     protected virtual void Solved()
