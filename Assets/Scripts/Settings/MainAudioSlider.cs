ff4d50a0c3570d6d0fabc9dc97ef94f1f0967ce2
Emilien-T <emilien.taisne@mail.mcgill.ca>
2024-01-21 12:49:10 -0500

    stuff

--- Assets/Scripts/Settings/MainAudioSlider.cs
+++ Assets/Scripts/Settings/MainAudioSlider.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class MainAudioSlider : MonoBehaviour
 {
+    public SoundMixerManager mixer;
+
     public void OnChangeMainVolume()
     {
-        float value = GetComponent<UnityEngine.UI.Slider>().value;
-        AudioSystem.Instance.SetMainVolume(value);
+        float value = GameObject.Find("MainVol").GetComponent<UnityEngine.UI.Slider>().value;
+        mixer.SetMasterVolume(value);
+    }
+
+    public void OnChangeBackgroundVolume()
+    {
+        float value = GameObject.Find("BackgroundVol").GetComponent<UnityEngine.UI.Slider>().value;
+        mixer.SetBackroundMusicVolume(value);
+    }
+    public void OnChangeSFXVolume()
+    {
+        float value = GameObject.Find("SFXVol").GetComponent<UnityEngine.UI.Slider>().value;
+        mixer.SetSoundFXVolume(value);
     }
 }
