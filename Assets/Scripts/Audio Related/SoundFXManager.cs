e0df1a964c76ae88428c71cb5e471544882b5eb6
averystraumann <averystraumann@icloud.com>
2024-01-21 09:44:10 -0500

    make sound more 3d hopefully

--- Assets/Scripts/Audio Related/SoundFXManager.cs
+++ Assets/Scripts/Audio Related/SoundFXManager.cs
@@ -8,6 +8,8 @@ public class SoundFXManager : MonoBehaviour
     public static SoundFXManager instance;
     public float volume;
     [SerializeField] private AudioSource soundFXObj;
+    [SerializeField] private float spread = 10;
+    [SerializeField] private float doppler = 4;
 
     // Start is called before the first frame update
     private void Awake() {
@@ -18,6 +20,8 @@ public class SoundFXManager : MonoBehaviour
 
     public void PlaySoundFX(AudioClip audioClip, Transform spawn, float volume) {
         AudioSource audioSource = Instantiate(soundFXObj, spawn.position, Quaternion.identity);
+        audioSource.spread = spread;
+        audioSource.dopplerLevel = doppler;
         audioSource.clip = audioClip;
         audioSource.volume = volume;
         audioSource.Play();
